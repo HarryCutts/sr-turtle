46d64c33c57809baeab7fc0b7432adc7d3895e18
Harry Cutts <eternal.linux@gmail.com>
2013-06-14 13:11:43 +0100

    Add markers to arena walls
    test.py updated to test this.

--- markers.py
+++ markers.py
@@ -1,8 +1,16 @@
 from game_object import GameObject
-from vision import create_marker_info_by_type, MARKER_TOKEN
+from vision import create_marker_info_by_type, MARKER_TOKEN, MARKER_ARENA
 
 class Token(GameObject):
     def __init__(s, arena, number):
         GameObject.__init__(s, arena)
         s.marker_info = create_marker_info_by_type(MARKER_TOKEN, number)
         s.surface_name = 'token.png'
+
+class WallMarker(GameObject):
+    def __init__(s, arena, number, location=(0,0), heading=0):
+        GameObject.__init__(s, arena)
+        s.marker_info = create_marker_info_by_type(MARKER_ARENA, number)
+        s.surface_name = 'wall_marker.png'
+        s.location = location
+        s.heading = heading
--- test.py
+++ test.py
@@ -10,3 +10,5 @@ print "I can see", len(markers), "markers:"
 for m in markers:
     if m.info.marker_type == MARKER_TOKEN:
         print " - Token {0} is {1} metres away".format(m.info.offset, m.dist)
+    if m.info.marker_type == MARKER_ARENA:
+        print " - Arena marker {0} is at {1} degrees".format(m.info.offset, m.rot_y)
