c1ee02630a026b9b81b27976f79da9a0a5f54489
Harry Cutts <eternal.linux@gmail.com>
2013-06-14 13:11:43 +0100

    Add markers to arena walls
    test.py updated to test this.

--- arena.py
+++ arena.py
@@ -2,6 +2,11 @@ from __future__ import division
 
 import pygame
 
+from markers import WallMarker
+from math import pi
+
+MARKERS_PER_WALL = 7
+
 class Arena(object):
 
     size = (6, 6)
@@ -15,8 +20,34 @@ class Arena(object):
     @property
     def bottom(s): return s.size[1] / 2
 
-    def __init__(s, objects=[]):
+    def __init__(s, objects=[], wall_markers=True):
         s.objects = objects
+        if wall_markers:
+            x_interval = s.size[0] / (MARKERS_PER_WALL + 1)
+            y_interval = s.size[1] / (MARKERS_PER_WALL + 1)
+            # Left wall
+            y = s.top + y_interval
+            for i in range(0, MARKERS_PER_WALL):
+                s.objects.append(WallMarker(s, i, (s.left, y), 0))
+                y += y_interval
+
+            # Bottom wall
+            x = s.left + x_interval
+            for i in range(MARKERS_PER_WALL, 2*MARKERS_PER_WALL):
+                s.objects.append(WallMarker(s, i, (x, s.bottom), pi / 2))
+                x += x_interval
+
+            # Right wall
+            y = s.bottom - y_interval
+            for i in range(2*MARKERS_PER_WALL, 3*MARKERS_PER_WALL):
+                s.objects.append(WallMarker(s, i, (s.right, y), pi))
+                y -= y_interval
+
+            # Top wall
+            x = s.right - x_interval
+            for i in range(3*MARKERS_PER_WALL, 4*MARKERS_PER_WALL):
+                s.objects.append(WallMarker(s, i, (x, s.top), 3 * pi / 2))
+                x -= x_interval
 
     ## Public Methods ##
 
