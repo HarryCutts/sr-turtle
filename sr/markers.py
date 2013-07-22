f83d9e88b5420f0881cc40e78e59654129af5aa6
Alastair Lynn <arplynn@gmail.com>
2013-07-22 19:36:05 +0100

    Minimise the amount of state kept by Token objects

--- sr/markers.py
+++ sr/markers.py
@@ -2,18 +2,22 @@ from game_object import GameObject
 from vision import create_marker_info_by_type, MARKER_TOKEN, MARKER_ARENA
 
 class Token(GameObject):
-    surface_name = 'sr/token.png'
     grabbable = True
 
     def __init__(self, arena, number):
         GameObject.__init__(self, arena)
         self.marker_info = create_marker_info_by_type(MARKER_TOKEN, number)
+        self.grabbed = False
 
     def grab(self):
-        self.surface_name = 'sr/token_grabbed.png'
+        self.grabbed = True
 
     def release(self):
-        self.surface_name = 'sr/token.png'
+        self.grabbed = False
+
+    @property
+    def surface_name(self):
+        return 'sr/token{0}.png'.format('_grabbed' if self.grabbed else '')
 
 class WallMarker(GameObject):
     surface_name = 'sr/wall_marker.png'
