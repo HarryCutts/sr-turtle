daec536d76f3128225249b064b31db6d7f4efb79
Alastair Lynn <arplynn@gmail.com>
2013-07-22 19:08:57 +0100

    Move to 'self' in arena.py

--- sr/arena.py
+++ sr/arena.py
@@ -27,47 +27,47 @@ class Arena(object):
     def bottom(self):
         return self.size[1] / 2
 
-    def __init__(s, objects=None, wall_markers=True):
-        s.objects = objects if objects is not None else []
+    def __init__(self, objects=None, wall_markers=True):
+        self.objects = objects if objects is not None else []
         if wall_markers:
-            x_interval = s.size[0] / (MARKERS_PER_WALL + 1)
-            y_interval = s.size[1] / (MARKERS_PER_WALL + 1)
+            x_interval = self.size[0] / (MARKERS_PER_WALL + 1)
+            y_interval = self.size[1] / (MARKERS_PER_WALL + 1)
             # Left wall
-            y = s.top + y_interval
+            y = self.top + y_interval
             for i in range(0, MARKERS_PER_WALL):
-                s.objects.append(WallMarker(s, i, (s.left, y), 0))
+                self.objects.append(WallMarker(self, i, (self.left, y), 0))
                 y += y_interval
 
             # Bottom wall
-            x = s.left + x_interval
+            x = self.left + x_interval
             for i in range(MARKERS_PER_WALL, 2*MARKERS_PER_WALL):
-                s.objects.append(WallMarker(s, i, (x, s.bottom), pi / 2))
+                self.objects.append(WallMarker(self, i, (x, self.bottom), pi / 2))
                 x += x_interval
 
             # Right wall
-            y = s.bottom - y_interval
+            y = self.bottom - y_interval
             for i in range(2*MARKERS_PER_WALL, 3*MARKERS_PER_WALL):
-                s.objects.append(WallMarker(s, i, (s.right, y), pi))
+                self.objects.append(WallMarker(self, i, (self.right, y), pi))
                 y -= y_interval
 
             # Top wall
-            x = s.right - x_interval
+            x = self.right - x_interval
             for i in range(3*MARKERS_PER_WALL, 4*MARKERS_PER_WALL):
-                s.objects.append(WallMarker(s, i, (x, s.top), 3 * pi / 2))
+                self.objects.append(WallMarker(self, i, (x, self.top), 3 * pi / 2))
                 x -= x_interval
 
     ## Public Methods ##
 
-    def contains_point(s, point):
+    def contains_point(self, point):
         x, y = point
-        if not (s.left < x < s.right):
-            return False, 0, max(s.left, min(x, s.right))
-        elif not (s.top < y < s.bottom):
-            return False, 1, max(s.top, min(y, s.bottom))
+        if not (self.left < x < self.right):
+            return False, 0, max(self.left, min(x, self.right))
+        elif not (self.top < y < self.bottom):
+            return False, 1, max(self.top, min(y, self.bottom))
         else:
             return True, None, None
 
-    def tick(s, time_passed):
-        for o in s.objects:
+    def tick(self, time_passed):
+        for o in self.objects:
             if hasattr(o, "tick"):
                 o.tick(time_passed)
