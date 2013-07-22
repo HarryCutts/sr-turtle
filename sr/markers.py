102381d3fb268b9f80c040fec1206f8e786f5322
Alastair Lynn <arplynn@gmail.com>
2013-07-22 19:34:30 +0100

    Make use of 'self' rather than 's'

--- sr/markers.py
+++ sr/markers.py
@@ -5,21 +5,22 @@ class Token(GameObject):
     surface_name = 'sr/token.png'
     grabbable = True
 
-    def __init__(s, arena, number):
-        GameObject.__init__(s, arena)
-        s.marker_info = create_marker_info_by_type(MARKER_TOKEN, number)
+    def __init__(self, arena, number):
+        GameObject.__init__(self, arena)
+        self.marker_info = create_marker_info_by_type(MARKER_TOKEN, number)
 
-    def grab(s):
-        s.surface_name = 'sr/token_grabbed.png'
+    def grab(self):
+        self.surface_name = 'sr/token_grabbed.png'
 
-    def release(s):
-        s.surface_name = 'sr/token.png'
+    def release(self):
+        self.surface_name = 'sr/token.png'
 
 class WallMarker(GameObject):
     surface_name = 'sr/wall_marker.png'
 
-    def __init__(s, arena, number, location=(0,0), heading=0):
-        GameObject.__init__(s, arena)
-        s.marker_info = create_marker_info_by_type(MARKER_ARENA, number)
-        s.location = location
-        s.heading = heading
+    def __init__(self, arena, number, location=(0,0), heading=0):
+        GameObject.__init__(self, arena)
+        self.marker_info = create_marker_info_by_type(MARKER_ARENA, number)
+        self.location = location
+        self.heading = heading
+
