1a34ecbd393a70dba5725106f2315ae998ce070a
Alastair Lynn <arplynn@gmail.com>
2013-07-22 19:26:23 +0100

    Change 's' to 'self' in display.py

--- sr/display.py
+++ sr/display.py
@@ -26,25 +26,25 @@ def get_surface(name):
 
 class Display(object):
 
-    def __init__(s, arena):
-        s.arena = arena
-        arena_w, arena_h = s.arena.size
-        s.size = (arena_w * PIXELS_PER_METER, arena_h * PIXELS_PER_METER)
+    def __init__(self, arena):
+        self.arena = arena
+        arena_w, arena_h = self.arena.size
+        self.size = (arena_w * PIXELS_PER_METER, arena_h * PIXELS_PER_METER)
 
         pygame.display.init()
-        s._window = pygame.display.set_mode(s.size)
+        self._window = pygame.display.set_mode(self.size)
         pygame.display.set_caption("SR Turtle Robot Simulator")
-        s._screen = pygame.display.get_surface()
-        s._draw_background()
-        s._draw()
+        self._screen = pygame.display.get_surface()
+        self._draw_background()
+        self._draw()
 
-    def __del__(s):
+    def __del__(self):
         pygame.display.quit()
 
-    def _draw_background(s):
-        s._background = pygame.Surface(s.size)
-        s._background.fill(ARENA_FLOOR_COLOR)
-        a = s.arena
+    def _draw_background(self):
+        self._background = pygame.Surface(self.size)
+        self._background.fill(ARENA_FLOOR_COLOR)
+        a = self.arena
         # Corners of the inside square
         top_left     = to_pixel_coord((a.left + a.zone_size, a.top + a.zone_size), a)
         top_right    = to_pixel_coord((a.right - a.zone_size, a.top + a.zone_size), a)
@@ -53,42 +53,42 @@ class Display(object):
 
         # Lines separating zones
         def line(start, end):
-            pygame.draw.line(s._background, ARENA_MARKINGS_COLOR, \
+            pygame.draw.line(self._background, ARENA_MARKINGS_COLOR, \
                              start, end, ARENA_MARKINGS_WIDTH)
 
         line((0, 0), top_left)
-        line((s.size[0], 0), top_right)
-        line(s.size, bottom_right)
-        line((0, s.size[1]), bottom_left)
+        line((self.size[0], 0), top_right)
+        line(self.size, bottom_right)
+        line((0, self.size[1]), bottom_left)
 
         # Square separating zones from centre
-        pygame.draw.polygon(s._background, ARENA_MARKINGS_COLOR, \
+        pygame.draw.polygon(self._background, ARENA_MARKINGS_COLOR, \
                             [top_left, top_right, bottom_right, bottom_left], 2)
 
         # Motif
         motif = get_surface(a.motif_name)
-        x, y = to_pixel_coord((0, 0), s.arena)
+        x, y = to_pixel_coord((0, 0), self.arena)
         w, h = motif.get_size()
-        s._background.blit(motif, (x - w / 2, y - h / 2))
+        self._background.blit(motif, (x - w / 2, y - h / 2))
 
-    def _draw(s):
-        s._screen.blit(s._background, (0, 0))
+    def _draw(self):
+        self._screen.blit(self._background, (0, 0))
 
-        for o in s.arena.objects:
+        for o in self.arena.objects:
             if o.surface_name != None:
                 with o.lock:
                     surface = get_surface(o.surface_name)
                     surface = pygame.transform.rotate(surface, -degrees(o.heading))
                     object_width, object_height = surface.get_size()
-                    x, y = to_pixel_coord(o.location, s.arena)
+                    x, y = to_pixel_coord(o.location, self.arena)
                     screen_location = (x - object_width / 2, y - object_height / 2)
-                    s._screen.blit(surface, screen_location)
+                    self._screen.blit(surface, screen_location)
 
         pygame.display.flip()
 
     ## Public Methods ##
 
-    def tick(s, time_passed):
-        s.arena.tick(time_passed)
+    def tick(self, time_passed):
+        self.arena.tick(time_passed)
         # TODO: Allow multiple displays on one arena without them all ticking it
-        s._draw()
+        self._draw()
