a286eff4f5a5b7861b4346432b0f0551506a7cd4
Alistair Lynn <arplynn@gmail.com>
2014-10-16 15:38:33 +0100

    Make the tokens physics objects

--- sr/markers.py
+++ sr/markers.py
@@ -1,13 +1,40 @@
 from game_object import GameObject
 from vision import create_marker_info_by_type, MARKER_TOKEN, MARKER_ARENA
 
+import pypybox2d
+
 class Token(GameObject):
     grabbable = True
 
+    @property
+    def location(self):
+        return self._body.position
+
+    @location.setter
+    def location(self, new_pos):
+        if self._body is None:
+            return # Slight hack: deal with the initial setting from the constructor
+        self._body.position = new_pos
+
+    @property
+    def heading(self):
+        return self._body.angle
+
+    @heading.setter
+    def heading(self, _new_heading):
+        if self._body is None:
+            return # Slight hack: deal with the initial setting from the constructor
+        self._body.angle = _new_heading
+
     def __init__(self, arena, number):
+        self._body = arena._physics_world.create_body(position=(0, 0),
+                                                      angle=0,
+                                                      fixed_rotation=True,
+                                                      type=pypybox2d.body.Body.DYNAMIC)
         GameObject.__init__(self, arena)
         self.marker_info = create_marker_info_by_type(MARKER_TOKEN, number)
         self.grabbed = False
+        self._body.create_circle_fixture(radius=0.1, density=1)
 
     def grab(self):
         self.grabbed = True
