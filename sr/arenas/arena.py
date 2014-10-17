3c7d6d7ed5b7b5af26798678ef3069999a43a9f4
Alistair Lynn <arplynn@gmail.com>
2014-10-17 12:43:59 +0100

    Correct the wall marker numbering
    This brings it in line with the diagram in the rules.

--- sr/arenas/arena.py
+++ sr/arenas/arena.py
@@ -53,16 +53,16 @@ class Arena(object):
     def _populate_wall_markers(self):
         # Left wall
         self._populate_wall(left = (self.left, self.bottom), right = (self.left, self.top),
-                            count = MARKERS_PER_WALL, start = 0, angle = 0)
+                            count = MARKERS_PER_WALL, start = 3*MARKERS_PER_WALL, angle = 0)
         # Right wall
-        self._populate_wall(left = (self.right, self.bottom), right = (self.right, self.top),
+        self._populate_wall(left = (self.right, self.top), right = (self.right, self.bottom),
                             count = MARKERS_PER_WALL, start = MARKERS_PER_WALL, angle = pi)
         # Bottom wall
-        self._populate_wall(left = (self.left, self.bottom), right = (self.right, self.bottom),
+        self._populate_wall(left = (self.right, self.bottom), right = (self.left, self.bottom),
                             count = MARKERS_PER_WALL, start = 2*MARKERS_PER_WALL, angle = pi / 2)
         # Top wall
         self._populate_wall(left = (self.left, self.top), right = (self.right, self.top),
-                            count = MARKERS_PER_WALL, start = 3*MARKERS_PER_WALL, angle = 3*pi / 2)
+                            count = MARKERS_PER_WALL, start = 0, angle = 3*pi / 2)
 
     def _init_physics(self):
         self._physics_world = pypybox2d.world.World(gravity=(0, 0))
