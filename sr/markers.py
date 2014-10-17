da807137bc55eaeb847b172ef69e158e915fa428
Alistair Lynn <arplynn@gmail.com>
2014-10-17 08:49:29 +0100

    Make tokens behave more realistically
    They now slow down and bounce off things.

--- sr/markers.py
+++ sr/markers.py
@@ -29,11 +29,16 @@ class Token(GameObject):
     def __init__(self, arena, number):
         self._body = arena._physics_world.create_body(position=(0, 0),
                                                       angle=0,
+                                                      linear_damping=0.5,
+                                                      angular_damping=0.5,
                                                       type=pypybox2d.body.Body.DYNAMIC)
         GameObject.__init__(self, arena)
         self.marker_info = create_marker_info_by_type(MARKER_TOKEN, number)
         self.grabbed = False
-        self._body.create_circle_fixture(radius=0.1, density=1)
+        self._body.create_circle_fixture(radius=0.1,
+                                         density=1,
+                                         restitution=0.2,
+                                         friction=0.3)
 
     def grab(self):
         self.grabbed = True
