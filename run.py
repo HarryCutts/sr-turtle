c7bc78433d266b4e35e2afb9600316a3a7cf88f0
Harry Cutts <eternal.linux@gmail.com>
2014-10-17 17:51:19 +0100

    Use argparse for run.py, and fall back to prompt

--- run.py
+++ run.py
@@ -1,19 +1,37 @@
 import sys
-from sr import *
 import yaml
 import threading
+import argparse
 from math import pi
 
-with open(sys.argv[1]) as f:
+from sr import *
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-c', '--config',
+                    type=argparse.FileType('r'),
+                    default='games/ctf.yaml')
+parser.add_argument('robot_scripts',
+                    type=argparse.FileType('r'),
+                    nargs='*')
+args = parser.parse_args()
+
+robot_scripts = args.robot_scripts
+prompt = "Enter the names of the Python files to run, separated by commas: "
+while not robot_scripts:
+    robot_script_names = raw_input(prompt).split(',')
+    if robot_script_names == ['']: continue
+    robot_scripts = [open(s.strip()) for s in robot_script_names]
+
+with args.config as f:
     config = yaml.load(f)
 
 sim = Simulator(config, foreground=True)
 
 class RobotThread(threading.Thread):
-    def __init__(self, zone, fn, *args, **kwargs):
+    def __init__(self, zone, script, *args, **kwargs):
         threading.Thread.__init__(self, *args, **kwargs)
         self.zone = zone
-        self.filename = fn
+        self.script = script
         self.setDaemon(True)
 
     def run(self):
@@ -23,11 +41,9 @@ class RobotThread(threading.Thread):
             robot_object.location = sim.arena.start_locations[self.zone]
             robot_object.heading = sim.arena.start_headings[self.zone]
             return robot_object
-        execfile(self.filename, {'Robot': robot})
+        exec self.script in {'Robot': robot}
 
-for zone, robot in enumerate(sys.argv[2:]):
-    if robot == 'empty':
-        continue
+for zone, robot in enumerate(robot_scripts):
     thread = RobotThread(zone, robot)
     thread.start()
 
